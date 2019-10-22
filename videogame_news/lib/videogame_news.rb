f5af611c62248fd39f207231f41889aed69e3c2d
Vanna Winland <vgwinland@gmail.com>
2019-10-22 20:30:37 +0000

    initial commit

---
+++ videogame_news/lib/videogame_news.rb
@@ -0,0 +1,6 @@
+require "videogame_news/version"
+
+module VideogameNews
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ videogame_news/lib/videogame_news/version.rb
@@ -0,0 +1,3 @@
+module VideogameNews
+  VERSION = "0.1.0"
+end
